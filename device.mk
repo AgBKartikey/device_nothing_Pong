e21e8a04e8eecb2c34dd5dc330cb55e19f86c4f7
Krzysztof Kosiński <krzysio@google.com>
2022-11-11 05:41:10 +0000

    Pong: Add prebuilt protobuf 3.9.1 to vendor partition.
    Needed to work around a Qualcomm HAL prebuilt.
    
    Bug: 203713560
    Bug: 258322017
    Bug: 254854421
    Test: Forrest
    Change-Id: I4eebc60f10fbea6a66127ed3e08441db32536f78
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -389,6 +389,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
 
