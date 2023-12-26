a7af3b09dab7bac849174fec4eeffe7fa7584ccc
AnierinBliss <anierin@evolution-x.org>
2023-12-26 22:12:10 +0530

    Pong: Introduce DeviceExtras
    Features:
    - PowerShare switch and Tile
    - More to come soon :)
    
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: Adrian DC <radian.dc@gmail.com>
    Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
    Co-authored-by: Michele Bono <bono.michele94@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
    Signed-off-by: HELLBOY017 <abhaygill017@gmail.com>

--- device.mk
+++ device.mk
@@ -143,6 +143,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# DeviceExtras
+PRODUCT_PACKAGES += \
+    Pong_DeviceExtras
+
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946639017191810.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946639017191810.xml
