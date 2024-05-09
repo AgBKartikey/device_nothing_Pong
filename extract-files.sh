0504896612e7a67da6509badef2711fbf8ab4652
Mir Yasin Ali <miryasin.miki@gmail.com>
2024-05-09 12:24:45 +0530

    [SQUASH]Pong: Nuke firmware images
    Revert "Pong: Allow extracting only firmware images"
    
    This reverts commit cf52016cf51478ee411f9f04d4e04c62aa2c2c77.
    
    Revert "Pong: Add firmware extraction support"
    
    This reverts commit f9134a3af72aeffc897eec14143514bde2dab28a.

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
+# Copyright (C) 2017-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -27,15 +27,11 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-ONLY_FIRMWARE=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-firmware )
-                ONLY_FIRMWARE=true
-                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -80,12 +76,6 @@ twt_congestion_timeout=0
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-if [ -z "${ONLY_FIRMWARE}" ]; then
-   extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ "${ONLY_FIRMWARE}" ]; then
-   extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
-fi
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
