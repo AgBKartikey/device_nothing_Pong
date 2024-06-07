a4aef8237c2fb1770cd2940cc0220ed2f81ca57f
gotenksIN <forumomkar@gmail.com>
2024-06-07 16:40:51 +0530

    Pong: Inherit Nothing camera
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,38 +4,44 @@ echo 'Starting vendorsetup processes needed for your device'
 rm -rf hardware/qcom-caf/sm8450/display
 rm -rf device/qcom/sepolicy_vndr/sm8450
 
-echo 'Cloning Vendor tree [1/6]'
+echo 'Cloning Vendor tree [1/7]'
 # vendor/nothing/Pong
 git clone https://gitlab.com/RealYasin/vendor_nothing_pong.git vendor/nothing/Pong
 
 echo 'DONE'
 
-echo 'Cloning Kernel tree [2/6]'
+echo 'Cloning Kernel tree [2/7]'
 # kernel/nothing/sm8475
 git clone --depth=1 https://github.com/Nothing-phone-2-Development/android_kernel_nothing_sm8475.git kernel/nothing/sm8475
 
 echo 'DONE'
 
-echo 'Cloning Display Hal [3/6]'
+echo 'Cloning Display Hal [3/7]'
 # hardware/qcom-caf/sm8450/display
 git clone --depth=1 https://github.com/Nothing-phone-2-Development/android_hardware_qcom_display.git hardware/qcom-caf/sm8450/display
 
 echo 'DONE'
 
-echo 'Cloning sepolicy_vndr [4/6]'
+echo 'Cloning sepolicy_vndr [4/7]'
 # device/qcom/sepolicy_vndr/sm8450
 git clone https://github.com/PongxViolet/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8450
 
 echo 'DONE'
 
-echo 'Cloning Dolby [5/6]'
+echo 'Cloning Dolby [5/7]'
 # hardware/dolby
 git clone https://github.com/FlamingoOS-Devices/hardware_dolby.git hardware/dolby
 
 echo 'DONE'
 
-echo 'Cloning PAGlyph Repo [6/6]'
+echo 'Cloning PAGlyph Repo [6/7]'
 # packages/apps/ParanoidGlyph
 git clone https://github.com/PongxViolet/android_packages_apps_ParanoidGlyph.git packages/apps/ParanoidGlyph
+
+echo 'DONE'
+
+echo 'Cloning NOSCamera [7/7]'
+# vendor/nothing/camera
+git clone https://gitlab.com/GhosutoX/vendor_nothing_camera.git vendor/nothing/camera
 
 echo 'Done and Completed'
