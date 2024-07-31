911ac492d9403416cc027824b6a67ce18dc71478
AgBKartikey <kartikey.np2@gmail.com>
2024-07-31 14:38:28 +0530

    Pong: Switch to own Forks

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,12 @@
 echo 'Cloning Vendor'
 # vendor/nothing/Pong
-git clone --depth=1 https://gitlab.com/chandu078/android_vendor_nothing_Pong.git vendor/nothing/Pong
+git clone --depth=1 https://gitlab.com/AgBKartikey/vendor-nothing-Pong.git vendor/nothing/Pong
 
 echo 'DONE'
 
 echo 'Cloning Kernel'
 # kernel/nothing/sm8475
-git clone --depth=1 https://github.com/Nothing-phone-2-Development/android_kernel_nothing_sm8475.git kernel/nothing/sm8475
+git clone --depth=1 https://github.com/AgBKartikey/android_kernel_nothing_sm8475.git kernel/nothing/sm8475
 
 echo 'DONE'
 
