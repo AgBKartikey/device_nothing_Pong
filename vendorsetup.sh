1c14418453ceade35b455afa39b79b4fa8d314d8
AgBKartikey <kartikey.np2@gmail.com>
2024-08-03 22:03:42 +0530

    Pong: Add Vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+echo 'Cloning Vendor'
+# vendor/nothing/Pong
+git clone --depth=1 https://gitlab.com/AgBKartikey/vendor_nothing_Pong.git vendor/nothing/Pong
+
+echo 'DONE'
+
+echo 'Cloning Kernel'
+# kernel/nothing/sm8475
+git clone --depth=1 https://github.com/AgBKartikey/android_kernel_nothing_sm8475.git kernel/nothing/sm8475
+
+echo 'DONE'
